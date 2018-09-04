f8c3d4ba5247e6b0790411c1f33688b18a11be4b
di0n0s <sfuentescarrique@gmail.com>
2018-09-04 16:45:20 +0200

    BaseActivity created

---
+++ presentation/src/main/java/com/example/sfuentes/speedruns/views/base/BaseActivity.kt
@@ -0,0 +1,35 @@
+package com.example.sfuentes.speedruns.views.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.sfuentes.speedruns.R
+import com.example.sfuentes.speedruns.extensions.inTransaction
+import kotlinx.android.synthetic.main.activity_base.*
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        setupActivityComponent()
+        addFragment(savedInstanceState)
+    }
+
+    private fun addFragment(savedInstanceState: Bundle?) =
+            savedInstanceState
+                    ?: supportFragmentManager.inTransaction { add(fragmentContainer.id, fragment()) }
+
+    override fun onBackPressed() {
+        (supportFragmentManager.findFragmentById(
+                fragmentContainer.id) as BaseFragment).onBackPressed()
+        super.onBackPressed()
+    }
+
+    private fun setupActivityComponent() {
+//        SpeedrunApplication
+//                .applicationComponent
+//                .inject(this)
+    }
+
+    abstract fun fragment(): BaseFragment
+}
