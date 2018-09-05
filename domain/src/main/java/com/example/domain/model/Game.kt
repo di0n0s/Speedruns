b396ce00f9c7259f27fc1cd820050c7486d0afd0
di0n0s <sfuentescarrique@gmail.com>
2018-09-05 17:01:42 +0200

    Game data class added

---
+++ domain/src/main/java/com/example/domain/model/Game.kt
@@ -0,0 +1,8 @@
+package com.example.domain.model
+
+data class Game(val id : String = "",
+                val name : String = "",
+                val logo : String = "",
+                val speedRunVideoUrl : String = "",
+                val playerName : String = "",
+                val speedRunTime : String = "")
