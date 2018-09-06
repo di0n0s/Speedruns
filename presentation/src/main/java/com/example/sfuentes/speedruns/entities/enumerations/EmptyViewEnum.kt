a9a8155634ec4611df386dfada29e41cb092c1eb
sfuentes <sfuentes@opentrends.net>
2018-09-06 15:14:29 +0200

    GameList empty view added

--- presentation/src/main/java/com/example/sfuentes/speedruns/entities/enumerations/EmptyViewEnum.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/entities/enumerations/EmptyViewEnum.kt
@@ -1,4 +1,7 @@
 package com.example.sfuentes.speedruns.entities.enumerations
 
+import com.example.sfuentes.speedruns.R
+
 enum class EmptyViewEnum(var imageId: Int, var title: Int, var subtitle: Int) {
+    EMPTY_GAME_LIST(R.drawable.ic_movie_placeholder, R.string.game_list_empty_title, R.string.game_list_empty_subtitle)
 }
