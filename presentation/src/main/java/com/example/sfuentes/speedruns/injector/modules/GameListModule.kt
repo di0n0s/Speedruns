4a53b9d7a2cd525abf0a88218c91184863432934
sfuentes <sfuentes@opentrends.net>
2018-09-06 13:25:09 +0200

    GameListActivityListener implemented

--- presentation/src/main/java/com/example/sfuentes/speedruns/injector/modules/GameListModule.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/injector/modules/GameListModule.kt
@@ -1,6 +1,7 @@
 package com.example.sfuentes.speedruns.injector.modules
 
 import com.example.sfuentes.speedruns.injector.PerFragment
+import com.example.sfuentes.speedruns.interfaces.GameListActivityListener
 import com.example.sfuentes.speedruns.views.gameList.GameListActivity
 import com.example.sfuentes.speedruns.views.gameList.GameListFragment
 import com.example.sfuentes.speedruns.views.gameList.GameListVew
@@ -8,12 +9,13 @@ import dagger.Module
 import dagger.Provides
 
 @Module
-class GameListModule (private val gameListFragment: GameListFragment,
-                      private val gameListActivity: GameListActivity) {
+class GameListModule(private val gameListFragment: GameListFragment,
+                     private val gameListActivity: GameListActivity) {
     @Provides
     @PerFragment
-    fun provideGameListView(): GameListVew {
-        return gameListFragment
-    }
+    fun provideGameListView(): GameListVew = gameListFragment
 
+    @Provides
+    @PerFragment
+    fun provideGameListActivityListener(): GameListActivityListener = gameListActivity
 }
--- presentation/src/main/java/com/example/sfuentes/speedruns/views/gameList/GameListActivity.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/views/gameList/GameListActivity.kt
@@ -1,12 +1,15 @@
 package com.example.sfuentes.speedruns.views.gameList
 
 import android.os.Bundle
+import android.view.View
 import com.example.sfuentes.speedruns.R
+import com.example.sfuentes.speedruns.entities.GameView
+import com.example.sfuentes.speedruns.interfaces.GameListActivityListener
 import com.example.sfuentes.speedruns.views.base.BaseActivity
 import com.example.sfuentes.speedruns.views.base.BaseFragment
 import kotlinx.android.synthetic.main.activity_base.*
 
-class GameListActivity : BaseActivity() {
+class GameListActivity : BaseActivity(), GameListActivityListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +20,9 @@ class GameListActivity : BaseActivity() {
 
     override fun setToolbarTitle() {
         toolbar.title = getString(R.string.game_list_toolbar_title)
+    }
+
+    override fun goToGameDetailActivity(gameView: GameView, view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
