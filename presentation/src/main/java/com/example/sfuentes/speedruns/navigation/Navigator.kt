ee86d4706d299a5a04882a73367913bd451b9d29
sfuentes <sfuentes@opentrends.net>
2018-09-06 20:43:44 +0200

    added function to navigator

--- presentation/src/main/java/com/example/sfuentes/speedruns/navigation/Navigator.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/navigation/Navigator.kt
@@ -1,14 +1,31 @@
 package com.example.sfuentes.speedruns.navigation
 
 import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.example.sfuentes.speedruns.R
 import com.example.sfuentes.speedruns.entities.GameView
+import com.example.sfuentes.speedruns.views.gameDetail.GameDetailActivity
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class Navigator @Inject constructor(){
+class Navigator @Inject constructor() {
 
-    fun toGameDetail(context: Context?, gameView: GameView, view: View){}
+    fun toGameDetail(context: Context?, gameView: GameView, view: View) {
+        if (context != null) {
+            val intentToLaunch = GameDetailActivity.getCallingIntent(context, gameView)
+            context.startActivity(intentToLaunch, prepareOptionsToBundle(context, view))
+        }
+    }
+
+    private fun prepareOptionsToBundle(context: Context?, view: android.view.View): Bundle? {
+        val transitionName = context?.getString(R.string.game_transition_poster)
+        val options = ActivityOptionsCompat
+                .makeSceneTransitionAnimation(context as AppCompatActivity, view, transitionName!!)
+        return options.toBundle()
+    }
 
 }
