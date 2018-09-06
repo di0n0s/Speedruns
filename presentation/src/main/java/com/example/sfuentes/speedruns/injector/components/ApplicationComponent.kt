d673d24595b355b8945f2b68f99d01d785bc911f
sfuentes <sfuentes@opentrends.net>
2018-09-06 15:22:31 +0200

    ApplicationComponent updated

--- presentation/src/main/java/com/example/sfuentes/speedruns/injector/components/ApplicationComponent.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/injector/components/ApplicationComponent.kt
@@ -1,14 +1,19 @@
 package com.example.sfuentes.speedruns.injector.components
 
+import com.example.sfuentes.speedruns.injector.modules.ApiModule
 import com.example.sfuentes.speedruns.injector.modules.ApplicationModule
+import com.example.sfuentes.speedruns.injector.modules.BaseListModule
+import com.example.sfuentes.speedruns.injector.modules.GameListModule
 import com.example.sfuentes.speedruns.views.base.BaseActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApplicationModule::class])
+@Component(modules = [ApplicationModule::class, ApiModule::class])
 interface ApplicationComponent {
 
     fun inject(baseActivity: BaseActivity)
+
+    fun plus(baseListModule: BaseListModule, gameListModule: GameListModule) : GameListComponent
 
 }
