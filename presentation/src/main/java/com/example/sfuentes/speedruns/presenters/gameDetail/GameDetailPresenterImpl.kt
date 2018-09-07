ec9ad99cf36ce20f2f312dbc0cf557fa8e14cad0
di0n0s <sfuentescarrique@gmail.com>
2018-09-07 12:33:35 +0200

    GameDetailFragment and Presenter done

--- presentation/src/main/java/com/example/sfuentes/speedruns/presenters/gameDetail/GameDetailPresenterImpl.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/presenters/gameDetail/GameDetailPresenterImpl.kt
@@ -1,36 +1,67 @@
 package com.example.sfuentes.speedruns.presenters.gameDetail
 
+import android.content.Context
+import com.example.domain.interactor.params.GameDetailParams
+import com.example.domain.interactor.usecase.GameDetailUseCase
+import com.example.domain.model.Game
 import com.example.sfuentes.speedruns.entities.GameView
+import com.example.sfuentes.speedruns.entities.mapper.GameViewMapper
+import com.example.sfuentes.speedruns.observers.GameDetailObserver
+import com.example.sfuentes.speedruns.views.gameDetail.GameDetailView
+import javax.inject.Inject
 
-class GameDetailPresenterImpl : GameDetailPresenter {
+class GameDetailPresenterImpl(private val gameDetailUseCase: GameDetailUseCase) : GameDetailPresenter {
 
+    @Inject
+    lateinit var context: Context
+    @Inject
+    lateinit var view : GameDetailView
     lateinit var gameView: GameView
 
-    override fun getGameDetail(gameId: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    private fun getGameDetail() {
+        view.showProgressBar(true)
+        gameDetailUseCase.execute(GameDetailParams(gameView.id), GameDetailObserver(this))
     }
 
     override fun setModel(gameView: GameView) {
         this.gameView = gameView
     }
 
+    override fun onGameDetailReceived(game: Game) {
+        setGameView(game)
+        view.setView()
+        setVideoButtonVisibility()
+        view.showProgressBar(false)
+    }
+
+    private fun setVideoButtonVisibility() {
+        if (gameView.speedRunVideoUrl.isNotBlank())
+            view.showButton()
+        else
+            view.hideButtton()
+    }
+
+    private fun setGameView(game: Game) {
+        gameView = GameViewMapper.toView(game, gameView, context)
+    }
+
     override fun onErrorReceived() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        view.showProgressBar(false)
+        view.showErrorMessage()
     }
 
     override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        getGameDetail()
     }
 
     override fun onResume() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onPause() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onDestroy() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        gameDetailUseCase.dispose()
     }
 }
--- presentation/src/main/java/com/example/sfuentes/speedruns/views/gameDetail/GameDetailFragment.kt
+++ presentation/src/main/java/com/example/sfuentes/speedruns/views/gameDetail/GameDetailFragment.kt
@@ -3,15 +3,24 @@ package com.example.sfuentes.speedruns.views.gameDetail
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.View
 import com.example.sfuentes.speedruns.R
+import com.example.sfuentes.speedruns.SpeedrunApplication
 import com.example.sfuentes.speedruns.entities.GameView
+import com.example.sfuentes.speedruns.injector.modules.GameDetailModule
+import com.example.sfuentes.speedruns.presenters.gameDetail.GameDetailPresenterImpl
 import com.example.sfuentes.speedruns.views.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_game_detail.*
+import javax.inject.Inject
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class GameDetailFragment : BaseFragment() {
+class GameDetailFragment : BaseFragment(), GameDetailView {
+
+    @Inject
+    lateinit var presenter : GameDetailPresenterImpl
 
     companion object {
         const val TAG = "GameDetailFragment"
@@ -29,7 +38,31 @@ class GameDetailFragment : BaseFragment() {
     override fun layoutId(): Int = R.layout.fragment_game_detail
 
     override fun setupFragmentComponent() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        SpeedrunApplication
+                .applicationComponent
+                .plus(GameDetailModule(this))
+                .inject(this)
+    }
+
+    override fun showProgressBar(show: Boolean) {
+        //TODO progressBar.visibility = View.GONE -- View.VISIBLE
+    }
+
+    override fun showErrorMessage() {
+        showToastMessage(getString(R.string.api_error))
+    }
+
+    override fun setView() {
+        playerNameTextView.text = presenter.gameView.playerName
+        runTimeTextView.text = presenter.gameView.speedRunTime
+    }
+
+    override fun showButton() {
+        videoButton.visibility = View.VISIBLE
+    }
+
+    override fun hideButtton() {
+        videoButton.visibility = View.GONE
     }
 
 
