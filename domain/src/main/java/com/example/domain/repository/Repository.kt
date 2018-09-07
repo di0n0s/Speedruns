8bd549811791eb3b11ca56ca3d9be30ae6fd3152
di0n0s <sfuentescarrique@gmail.com>
2018-09-07 11:39:23 +0200

    Repository done

--- domain/src/main/java/com/example/domain/repository/Repository.kt
+++ domain/src/main/java/com/example/domain/repository/Repository.kt
@@ -1,9 +1,12 @@
 package com.example.domain.repository
 
+import com.example.domain.model.Game
 import com.example.domain.model.GameListPagination
 import io.reactivex.Observable
 
 interface Repository {
 
     fun getGameList(offset: Int): Observable<GameListPagination>
+
+    fun getGameDetail(gameId: String): Observable<Game>
 }
